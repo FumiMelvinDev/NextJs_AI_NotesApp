9445af72d911b725c5aab938179aee1913c8a557
FumiMelvinDev <fumimelvindev@gmail.com>
2025-05-21 19:36:34 +0200

    Add Sonner toast notifications and update logout functionality

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -3,12 +3,27 @@
 import { Loader2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const LogoutButton = () => {
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-  const handleLogout = () => {
-    console.log("Logging out...");
+  const handleLogout = async () => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const errMessage = null;
+
+    if (!errMessage) {
+      toast("Logout successful");
+
+      router.push("/");
+    } else {
+      toast.error(errMessage);
+    }
+    setLoading(false);
   };
   return (
     <Button onClick={handleLogout} disabled={loading} className="w-20">
