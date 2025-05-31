ff9c605a2c51138aeae2b4df18ce4b813956f514
FumiMelvinDev <fumimelvindev@gmail.com>
2025-05-31 19:32:36 +0200

    feat: implement new UI components for note management and integrate user authentication

---
+++ src/components/AskAiButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { User } from "@supabase/supabase-js";
+
+type Props = {
+  user: User | null;
+};
+
+const AskAIButton = ({ user }: Props) => {
+  console.log(user?.email);
+  return <div className="">AskAIButton</div>;
+};
+
+export default AskAIButton;
---
+++ src/components/NoteTextInput.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type Props = {
+  noteId: string;
+  startingNoteText: string;
+};
+
+const NoteTextInput = ({ noteId, startingNoteText }: Props) => {
+  return <div className="">Note input</div>;
+};
+
+export default NoteTextInput;
